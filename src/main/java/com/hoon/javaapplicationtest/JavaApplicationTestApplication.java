e85ef5b51ad64bacf8fefe5a51def5ffc12c7e47
김광훈 <60383031+Wave1994-Hoon@users.noreply.github.com>
2021-09-16 00:10:27 +0900

    initial commit

---
+++ src/main/java/com/hoon/javaapplicationtest/JavaApplicationTestApplication.java
@@ -0,0 +1,13 @@
+package com.hoon.javaapplicationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApplicationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApplicationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hoon/javaapplicationtest/JavaApplicationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoon.javaapplicationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
